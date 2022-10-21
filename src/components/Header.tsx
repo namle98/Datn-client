4aa46bc6ace4fa950eca9672e310bd78d34b9c0e
namle98 <lephuongnam2807@gmail.com>
2022-10-21 23:14:54 +0700

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,33 @@
 import { Menu } from "antd";
-import { useState } from "react";
-
-const { SubMenu } = Menu;
+import { useEffect, useState } from "react";
+import { HomeOutlined, UserAddOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, useLocation } from "react-router-dom";
+const { Item } = Menu;
 function Header() {
   const [current, setCurrent] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") setCurrent("home");
+    if (location.pathname === "/login") setCurrent("login");
+    if (location.pathname === "/register") setCurrent("register");
+  }, [location.pathname]);
+
+  const handleSwitchMenu = (e: any) => {
+    setCurrent(e.key);
+  };
+
   return (
-    <Menu selectedKeys={[current]} mode="horizontal">
-      <Menu.Item>Login</Menu.Item>
-      <Menu.Item>Register</Menu.Item>
-      <Menu.Item>Home</Menu.Item>
+    <Menu onClick={handleSwitchMenu} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<HomeOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
+      <Item key="login" icon={<UserOutlined />}>
+        <Link to="/login">Login</Link>
+      </Item>
+      <Item key="register" icon={<UserAddOutlined />}>
+        <Link to="/register">Register</Link>
+      </Item>
     </Menu>
   );
 }
