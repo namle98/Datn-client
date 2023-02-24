f96dff06047f6e854637f6de3d399684e57bb7f4
namle98 <lephuongnam2807@gmail.com>
2023-02-24 18:20:23 +0700

    fix bug dashboard

--- src/pages/admin/dashboard/revenue.tsx
+++ src/pages/admin/dashboard/revenue.tsx
@@ -12,19 +12,19 @@ function Revenue({ totalAmount, money }: any) {
       <Card style={{ width: "200px" }}>
         <Space direction="horizontal">
           <CreditCardOutlined style={{ fontSize: "20px" }} />
-          <Statistic title="Card" value={`$${money("card")}`} />
+          <Statistic title="Card" value={`$${money("card") || 0}`} />
         </Space>
       </Card>
       <Card style={{ width: "200px" }}>
         <Space direction="horizontal">
           <PayCircleOutlined style={{ fontSize: "20px" }} />
-          <Statistic title="Cash" value={`$${money("cash")}`} />
+          <Statistic title="Cash" value={`$${money("cash") || 0}`} />
         </Space>
       </Card>
       <Card style={{ width: "200px" }}>
         <Space direction="horizontal">
           <DollarCircleOutlined style={{ fontSize: "20px" }} />
-          <Statistic title="Total" value={`$${totalAmount()}`} />
+          <Statistic title="Total" value={`$${totalAmount() || 0}`} />
         </Space>
       </Card>
     </Space>
