578992be934aadb7bdc92558f97e5b071fd61a84
namle98 <lephuongnam2807@gmail.com>
2023-04-17 20:07:20 +0700

    update axios

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -8,6 +8,18 @@ class Http {
         "Content-Type": "application/json",
       },
     });
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        console.log("error", error);
+        if (error.response.status === 401) window.location.href = "/login";
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+      }
+    );
   }
 }
 
